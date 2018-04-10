e510fbb517e62f6cf5aa5be006acf239b5e66fb7
yancongwen <ycwen1119@163.com>
2018-04-10 17:16:32 +0800

    用bash脚本创建目录并写文件

--- demo.sh
+++ demo.sh
@@ -8,7 +8,7 @@ else
   touch index.html css/style.css js/main.js
   echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>">>index.html
   echo -e "h1{color: red;}">>css/style.css
-  echo -e "var string = 'Hello World'\nalert(string)">>js/main.js
+  echo -e "var string = 'Hello World!'\nalert(string)">>js/main.js
   echo 'success'
   exit 0
 fi
